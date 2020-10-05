8ab30e37aed0404649b511a01272c0939dfcb8ca
iTh1nk <fnchao@hotmail.com>
2020-10-04 21:49:49 -0700

    fix intro intro_id

--- api/Intro/urls.py
+++ api/Intro/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('get/', views.GetAll.as_view()),
     path('post/', views.PostAll.as_view()),
-    path('delete/<uuid:post_id>/', views.DeleteAny.as_view())
+    path('delete/<uuid:intro_id>/', views.DeleteAny.as_view())
 ]
