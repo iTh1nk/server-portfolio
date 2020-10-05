548d22db51bc4df2a8d8a8d724cc729667f8991b
iTh1nk <fnchao@hotmail.com>
2020-10-04 21:58:56 -0700

    fix intro intro_id

--- api/messages/urls.py
+++ api/messages/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('get/', views.GetAll.as_view()),
-    path('get/<uuid:post_id>/', views.GetAny.as_view()),
+    path('get/<uuid:message_id>/', views.GetAny.as_view()),
     path('get/page/', views.GetPage.as_view()),
     path('post/', views.PostAll.as_view()),
-    path('delete/<uuid:post_id>/', views.DeleteAny.as_view())
+    path('delete/<uuid:message_id>/', views.DeleteAny.as_view())
 ]
